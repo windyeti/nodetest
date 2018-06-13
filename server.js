3ed4cbed3c9c280f9f2709a2921866463ec653ee
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-06-13 11:48:50 +0300

    add body-parser

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use( bodyParser.json() );
+
 app.get('/', (req, res) => {
     res.send('<h4>It is response when you request GET!!!</h4>');
+});
+app.post('/post', (req, res) => {
+    res.json(req.body)
 });
 
 app.listen(8080, (err) => {
