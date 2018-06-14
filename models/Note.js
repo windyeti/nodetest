676dd810e053f7238e8bf9d21049c0ee0495bf37
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-06-14 07:42:53 +0300

    change server.js -> add Note.count() to APP.GET

--- models/Note.js
+++ models/Note.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
-const getConnection = () => {
-    return mongoose.connect('localhost:8080/note')
-};
 const NoteSchema = mongoose.Schema({
     title : String,
     text : String,
     color : String
 });
 
-const Note = mongoose.model('Note', NoteSchema);
+export const Note = mongoose.model('Note', NoteSchema);
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('localhost/listnews', (err) => {
+    if(err) {
+        console.log('connect db-listnews error', err)
+    } else {
+        console.log('connect db-listnews success');
+    }
+});
+
+const Note = require('./models/Note');
 
 const app = express();
 
 app.use( bodyParser.json() );
 
 app.get('/', (req, res) => {
+    Note.count().then(result => {
+        console.log('result', result);
+    });
     res.send('<h4>It is response when you request GET!!!</h4>');
 });
 app.post('/post', (req, res) => {
